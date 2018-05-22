c5144f65089c099747c142f4590ca0ea9e1516da
Jeiner Noriega <bigal2331@yahoo.com>
2018-05-21 21:58:21 -0400

    added Posts component

---
+++ components/Posts.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+
+class Posts extends Component {
+
+	constructor(props){
+
+		super(props);
+		this.state = {
+			posts: []
+		};
+	}
+
+	componentWillMount(){
+		fetch('https://jsonplaceholder.typicode.com/posts')
+		.then(res => res.json())
+		.then(data => this.setState({
+			posts: data
+		}));
+	}
+	render(){
+		return(
+			<div>
+				<h1>What up!!!</h1>
+			</div>
+		)
+	}
+
+
+}
+
+
+export default Posts;
